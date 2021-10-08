65da93366b6294e1d10e5182aea5a2b4c2e28522
Duy Trong <tranduytrong2000@gmail.com>
2021-10-08 16:33:41 +0700

    building core - not finish

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+const express = require("express")
+const model = require("./model/DB_Access_Helper")
+
+const app = express();
+
+let port = process.env.port || 3000;
+
+app.get("/", function (req, res) {
+    res.send("API is running...");
+});
+
+app.get("/test", (req,res)=>{
+    let connect = model.connect();
+    res.status(200).send({message:"connect success"})
+})
+
+app.listen(port, ()=>{
+    console.log(`app is listening on ${port}`)
+})
