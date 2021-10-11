496d73102051ad764eff032868043cffecbafb2e
Duy Trong <tranduytrong2000@gmail.com>
2021-10-11 16:18:19 +0700

    fix hard-code at sequelize config

--- model/model.js
+++ model/model.js
@@ -1,31 +1,21 @@
 const Sequelize = require('sequelize');
 
-let sequelizeConfig = {
-    HOST: "my-database-for-node-app",
-    PORT: process.env.DB_PORT,
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    operatorsAliases: false
-}   
 module.exports = function testSel(){
-    const sequelize = new Sequelize('BookStore', 'Trong', 'Trong2000', {
-        host: 'my-database-for-node-app',
-        dialect: 'mysql',
-      
-        pool: {
-          max: 5,
-          min: 0,
-          acquire: 30000,
-          idle: 10000
-        },      
-        // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
-        operatorsAliases: false
-      });
-
+    const sequelize = new Sequelize(
+        process.env.DB_NAME, 
+        process.env.DB_USER, 
+        process.env.DB_PASSWORD, {
+            host: 'my-database-for-node-app',
+            dialect: 'mysql', 
+            pool: {
+                max: 5,
+                min: 0,
+                acquire: 30000,
+                idle: 10000
+            },      
+            // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+            operatorsAliases: false
+        }
+    );
 }
 
