824fd7fbbf7ca42d1a588d6fecb0de5abdef7a94
trong-tma <tdtrong.tma@outlook.com>
2022-03-11 10:54:34 +0700

    change be code

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,11 @@ const book = require('./book.routes');
 const shop = require('./shop.routes');
 const transaction = require('./transaction.routes');
 
-router.use('/api/account', account);
-
-// Api author, category
-router.use('/api/author', author);
-router.use('/api/category', category);
-
-// Api book (include book_author, book_category)
-router.use('/api/book', book);
-
-// Shop, payment, shipper, coupon api
-router.use('/api/shop', shop);
-
-// Transaction
-router.use('/api/transaction', transaction);
+router.use('/account', account);
+router.use('/author', author);
+router.use('/category', category);
+router.use('/book', book);
+router.use('/shop', shop);
+router.use('/transaction', transaction);
 
 module.exports = router;
