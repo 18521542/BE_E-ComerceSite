8016167da1aad730fab2aebf1fccf6b40de7858f
linhleba <linhleba.it@gmail.com>
2022-03-19 11:05:57 +0700

    update status for transaction

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const TransactionController = require('../controller/transaction.controller');
 const JWT = require('../middleware/auth');
+const { route } = require('./author.routes');
 const router = express.Router();
 
 const controller = new TransactionController();
 
-router.get('/', controller.action('list'));
+router.get(
+  '/',
+  JWT.verifyRoleAdmin(process.env.ACCESS_JWT_SECRET),
+  controller.action('list'),
+);
 router.get(
   '/history',
   JWT.verifyJWT(process.env.ACCESS_JWT_SECRET),
@@ -15,6 +20,11 @@ router.post(
   '/',
   JWT.verifyJWT(process.env.ACCESS_JWT_SECRET),
   controller.action('create'),
+);
+router.put(
+  '/updatestatus/:id',
+  JWT.verifyJWT(process.env.ACCESS_JWT_SECRET),
+  controller.updateStatus,
 );
 
 module.exports = router;
