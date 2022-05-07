ddd755a7295729c72027a601aeb374dcf1d01c35
Duy Trong <tdtrong.tma@outlook.com>
2022-05-07 10:38:51 +0700

    config ml api

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const category = require('./category.routes');
 const book = require('./book.routes');
 const shop = require('./shop.routes');
 const transaction = require('./transaction.routes');
+const fetch = require("cross-fetch")
+// fetch
+
 
 router.use('/account', account);
 router.use('/author', author);
@@ -13,5 +16,35 @@ router.use('/category', category);
 router.use('/book', book);
 router.use('/shop', shop);
 router.use('/transaction', transaction);
+
+var mlAPI = 'default';
+router.get('/ML_API_URL', async (req, res) => {
+    let flagAPI = req.body.mlURL;
+    mlAPI = flagAPI;
+    return res.send({
+        result: mlAPI,
+        message: "Get URL success"
+    })
+})
+
+router.get(`/MLData`, async (req, res) => {
+    console.log("here", mlAPI)
+    if(mlAPI != 'default'){
+        let requestToMLServer = await fetch(mlAPI, {
+            "Content-type": "application/json"
+        });
+        let result = await requestToMLServer.json();
+        return res.send({
+            message: "you have config ml api",
+            ML_API_URL: mlAPI,
+            yourData: result
+        })
+    }
+
+    return res.send({
+        ml_url: mlAPI,
+        message: "get haven't ml url success"
+    })
+})
 
 module.exports = router;
